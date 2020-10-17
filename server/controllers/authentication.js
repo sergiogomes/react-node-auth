5edd928262ee0f5d789235ea8b6ef0e364d2f347
sergiogomes <sergiopgomes92@gmail.com>
2020-10-16 22:09:01 -0300

    Handling signup authentication, and creating new user on mongo.

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,31 @@
+const User = require("../models/user");
+
 exports.signup = (req, res, next) => {
-  res.send({ success: true });
+  const { email, password } = req.body;
+
+  // See if a user with th given email exists
+  User.findOne({ email: email }, (err, existingUser) => {
+    if (err) {
+      return next(err);
+    }
+
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: "Email is in use." });
+    }
+
+    // If a user with email dow NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password,
+    });
+    user.save((err) => {
+      if (err) {
+        return next(err);
+      }
+
+      // Respond to request indicating the user was created
+      res.json({ success: true });
+    });
+  });
 };
