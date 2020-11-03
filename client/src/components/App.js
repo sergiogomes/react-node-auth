0b6a2da2a7a18917baf2f22b5c2b3255a45b9785
sergiogomes <sergiopgomes92@gmail.com>
2020-11-02 23:16:57 -0300

    Routing with children.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import Header from "./Header";
 
-const App = () => {
+const App = ({ children }) => {
   return (
     <div>
       <Header />
       <div>Hi</div>
+      {children}
     </div>
   );
 };
