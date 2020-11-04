0799d04f3e0d8db3d2246b899b144d78c14f4950
sergiogomes <sergiopgomes92@gmail.com>
2020-11-03 23:50:52 -0300

    Starting the signup page.

---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <form>
+      <fieldset>
+        <label>Email</label>
+      </fieldset>
+      <fieldset>
+        <label>Password</label>
+      </fieldset>
+    </form>
+  );
+};
+
+export default Signup;
